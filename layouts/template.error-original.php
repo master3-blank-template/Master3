115df0e102fbacbefce6648176ff592be22a670a
AlekVolsk <alekvolsk@yandex.ru>
2019-08-18 22:56:34 +0400

    v1.1.22

--- layouts/template.error-original.php
+++ layouts/template.error-original.php
@@ -10,12 +10,15 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
+
+$errorLevelStr = Factory::getConfig()->get('error_reporting', 'default');
+$isTable = ($errorLevelStr === 'maximum') || ($errorLevelStr === 'development');
+$isBacktrace = $errorLevelStr === 'development';
 
 $errorCode = $this->error->getCode();
 
-$link = Uri::base(true) . Route::_(Factory::getApplication()->getMenu('site')->getDefault()->link);
+$link = Uri::base(true);
 
 ?>
 <!DOCTYPE html>
@@ -57,7 +60,7 @@ $link = Uri::base(true) . Route::_(Factory::getApplication()->getMenu('site')->g
                 
                 <div class="uk-margin-large-top"><?php echo Text::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?></div>
 
-                <?php if ($this->debug) { ?>
+                <?php if ($isTable) { ?>
                 <table class="uk-table uk-table-divider uk-table-striped uk-margin uk-table-responsive uk-margin-large-top">
                     <tr>
                         <td class="uk-text-bold">Error Code</td>
@@ -72,6 +75,9 @@ $link = Uri::base(true) . Route::_(Factory::getApplication()->getMenu('site')->g
                         <td><?php echo htmlspecialchars($this->error->getFile(), ENT_QUOTES, 'UTF-8'), ':', $this->error->getLine(); ?></td>
                     </tr>
                 </table>
+                <?php } ?>
+                
+                <?php if ($isBacktrace) { ?>
                 <div class="uk-margin-large-top">
                     <?php
                     echo $this->renderBacktrace();
