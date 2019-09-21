c54275c7d00951642e04fc3832d0ba5f41a62e2e
AlekVolsk <alekvolsk@yandex.ru>
2019-09-21 17:42:41 +0400

    1.1.26

--- html/mod_breadcrumbs/default.php
+++ html/mod_breadcrumbs/default.php
@@ -8,6 +8,8 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Uri\Uri;
+
 echo '<ul class="uk-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
 
 if (!$params->get('showLast', 1)) {
@@ -15,6 +17,8 @@ if (!$params->get('showLast', 1)) {
 }
 
 $count = count($list);
+$linkCurrent = Uri::current();
+$linkRoot = substr(Uri::root(), 0, -1);
 
 for ($i = 0; $i < $count; $i++) {
     if ($i == 1 && !empty($list[$i]->link) && !empty($list[$i - 1]->link) && $list[$i]->link == $list[$i - 1]->link) {
@@ -32,12 +36,12 @@ for ($i = 0; $i < $count; $i++) {
     if ($i < $count - 1) {
         if (!empty($list[$i]->link)) {
             $link = explode('?', $list[$i]->link)[0];
-            echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><a href="' . $link . '" itemprop="item"><span itemprop="name">' . $name . '</span></a>' . $meta . '</li>';
+            echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><a itemprop="item" content="' . $linkRoot . $link . '" href="' . $link . '"><span itemprop="name">' . $name . '</span></a>' . $meta . '</li>';
         } else {
-            echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><span itemprop="item"><span itemprop="name">' . $name . '</span></span>' . $meta . '</li>';
+            echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><span itemprop="item" content="' . $linkRoot . $link . '"><span itemprop="name">' . $name . '</span></span>' . $meta . '</li>';
         }
     } else {
-        echo '<li class="uk-active" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><span itemprop="item"><span itemprop="name">' . $name . '</span></span>' . $meta . '</li>';
+        echo '<li itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem"><span itemprop="item" content="' . $linkCurrent . '"><span itemprop="name">' . $name . '</span></span>' . $meta . '</li>';
     }
 }
 
