b7570a45beceaadfd2aeb34704a7b49b65455c8a
AlekVolsk <alekvolsk@yandex.ru>
2020-04-15 23:51:55 +0400

    v1.2.14

--- html/layouts/joomla/content/full_image.php
+++ html/layouts/joomla/content/full_image.php
@@ -14,11 +14,12 @@ $images = json_decode($displayData->images);
 
 if (isset($images->image_fulltext) && !empty($images->image_fulltext)) {
     $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext;
+    $alt = $images->image_fulltext_alt ?: $displayData->title;
     ?>
-<div class="uk-align-<?php echo htmlspecialchars($imgfloat); ?> item-image">
+<div class="uk-align-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
     <img <?php if ($images->image_fulltext_caption) {
-            echo 'class="caption" title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
-        } ?> src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/>
+            echo 'class="caption" title="' . htmlspecialchars($images->image_fulltext_caption, ENT_COMPAT, 'UTF-8') . '"';
+        } ?> src="<?php echo htmlspecialchars($images->image_fulltext, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($alt, ENT_COMPAT, 'UTF-8'); ?>" itemprop="image"/>
 </div>
 <?php
 }
--- html/layouts/joomla/content/intro_image.php
+++ html/layouts/joomla/content/intro_image.php
@@ -16,17 +16,18 @@ $images = json_decode($displayData->images);
 
 if (isset($images->image_intro) && !empty($images->image_intro)) {
     $imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro;
+    $alt = $images->image_intro_alt ?: $displayData->title;
 ?>
 <div class="uk-align-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
     <?php if ($params->get('link_titles') && $params->get('access-view')) { ?>
     <a href="<?php echo Route::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
         <img <?php if ($images->image_intro_caption) {
                 echo 'class="caption" title="' . htmlspecialchars($images->image_intro_caption) . '"';
-            } ?> src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>" itemprop="thumbnailUrl"/></a>
+            } ?> src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($alt, ENT_COMPAT, 'UTF-8'); ?>" itemprop="thumbnailUrl"/></a>
     <?php } else { ?>
     <img <?php if ($images->image_intro_caption) {
             echo 'class="caption" title="' . htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8') . '"';
-        } ?> src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>" itemprop="thumbnailUrl"/>
+        } ?> src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($alt, ENT_COMPAT, 'UTF-8'); ?>" itemprop="thumbnailUrl"/>
     <?php } ?>
 </div>
 <?php
